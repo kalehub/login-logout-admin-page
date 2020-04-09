add92dbf44da1511f24eb93023cf4b3108bcd0a5
kalehub <62944462+kalehub@users.noreply.github.com>
2020-04-09 21:18:20 +0800

    Update signup.inc.php

--- bem_dash/includes/signup.inc.php
+++ bem_dash/includes/signup.inc.php
@@ -14,7 +14,7 @@ if (isset($_POST['signup-submit'])) {
         header("Location: ../signup.php?error=emptyfields&uid=".$username."&mail".$email);
         exit();
     }elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        header("Location: ../signup.php?error=invalidmailuid");
+        header("Location: ../signup.php?error=invalidmail&uid=".$username);
         exit();
     }
     elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
